f47fd048084dce4a31b564b85f3bc803f965bd9c
Travis Kaufman <travis.kaufman@gmail.com>
2016-10-14 11:28:24 -0400

    Update model with more Lagasse. Longer sentences

--- pepperonibot/cli.py
+++ pepperonibot/cli.py
@@ -7,7 +7,7 @@ def run():
     num_sentences = random.randint(1, 3)
     model = get_model()
     for _ in range(num_sentences):
-        print model.make_short_sentence(200)
+        print model.make_short_sentence(300)
 
 
 if __name__ == '__main__':
